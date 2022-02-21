e12d4c3b5518a270744dccf0cc19cb5ee8e86df3
lauraalyson <lauraalyson3@gmail.com>
2022-02-21 15:42:43 -0500

    new fonts and styles for global text

--- src/App.js
+++ src/App.js
@@ -6,39 +6,33 @@ function App() {
   return (
 		<div className='App'>
 			<header className='App-header'>Moody</header>
+
+			{/* Search Bar */}
+			<SearchBar />
+
+			{/* Results Grid */}
+			<ResultsGrid />
+
+			{/* Results Loose Shapes */}
+
 			<body>
-
 				<ul>Functionality includes:</ul>
-          <li>search for a location</li>
-          <li>get current time</li>
-          <li>get current weather</li>
-          <li>get suggested playlist</li>
-          <li>top news headline from currLocation</li>
-
+				<li>search for a location</li>
+				<li>get current time</li>
+				<li>get current weather</li>
+				<li>get suggested playlist</li>
+				<li>top news headline from currLocation</li>
+				<li>Toggle structure - grid to loose shapes</li>
 
 				<ul>Imports + Integrations</ul>
-          <li>React Grid Draggable</li>
-          <li>Current Time</li>
-          <li>isMobile ? NPM install</li>
-          <li>Spotify API</li>
-          <li>Open Weather API</li>
-          <li>Current News API</li>
-
+				<li>React Grid Draggable</li>
+				<li>Current Time</li>
+				<li>isMobile ? NPM install</li>
+				<li>Spotify API</li>
+				<li>Open Weather API</li>
+				<li>Current News API</li>
 			</body>
-
-      {/* Search Bar */}
-      <SearchBar />
-
-      {/*  */}
-      <ResultsGrid />
-
-      {/*  */}
-      
-      <footer>
-        <p>About</p>
-        <p>Documentation</p>
-        <p>License</p>
-      </footer>
+			<footer></footer>
 		</div>
 	)
 }
