a51c1a784d03fe77e8c3fb3bc3829d759de57462
lauraalyson <lauraalyson3@gmail.com>
2022-02-21 12:40:50 -0500

    list functionality, integrations and imports

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Moody</header>
-    </div>
-  );
+		<div className='App'>
+			<header className='App-header'>Moody</header>
+			<body>
+
+				<ul>Functionality includes:</ul>
+          <li>search for a location</li>
+          <li>get current time</li>
+          <li>get current weather</li>
+          <li>get suggested playlist</li>
+          <li>top news headline from currLocation</li>
+
+
+				<ul>Imports + Integrations</ul>
+          <li>React Grid Draggable</li>
+          <li>Current Time</li>
+          <li>isMobile ? NPM install</li>
+          <li>Spotify API</li>
+          <li>Open Weather API</li>
+          <li>Current News API</li>
+
+			</body>
+		</div>
+	)
 }
 
 export default App;
