c5c0fd8f300f89b57d18cde423fd7b21e832eda8
lauraalyson <lauraalyson3@gmail.com>
2022-03-04 13:49:18 -0500

    successful news call, store image url, headline, author, source

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,25 +1,34 @@
 import React, {useState} from 'react'
-import { getWeather } from '../api/apiConfig'
+import { getWeather, getNews } from '../api/apiConfig'
 
 function SearchBar() {
 	const [results, setResults] = useState('')
 	const [location, setLocation] = useState('')
+	const [news, setNews] = useState('');
 
 	const search = () => {
 		getWeather(location)
 			.then(console.log('Search function response ' + getWeather(location)))
 			.then((res) => setResults(res.data.weather[0].description))
-			.then(() => console.log('Results: ' + results))
+			.then(() => setLocation(''))
+			.catch((err) => console.log(err))
+
+		getNews(location)
+			.then((res) => setNews(res.data.articles[0]))
+			.catch((err) => console.log(err))
 	}
 
 	return (
 		<div className='search-bar'>
             <label>Search Bar</label>
-            <input onChange={(event) => setLocation(event.target.value)}/>
+            <input onChange={(event) => setLocation(event.target.value)} placeholder='test' />
 			<p onClick={() => search()}>Submit</p>
 			{/* <button onClick={() => search()}>Search</button> */}
 			<p>Location: {location}</p>
-			<p>Results: {results}</p>
+			<p>Weather: {results}</p>
+			<p>News Headline: {news.title}</p>
+			<p>Author: {news.author}</p>
+			<img style={{ maxWidth: '80%' }} src={news.urlToImage} alt={news.title}/>
 		</div>
 	)
 }
--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,16 +1,26 @@
-import axios from 'axios'
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
-const apiKey = '1c5a1c2a7a89bc7970d716efa8525e1e'
+import axios from 'axios';
+const weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const weatherApiKey = '1c5a1c2a7a89bc7970d716efa8525e1e';
 
-// Weather Endpoints
-export const getWeather = (city) => {
+const newsApiUrl = 'https://newsapi.org/v2/everything?q='
+const newsApiKey = '&apiKey=036c3c402b6c4e14bde7779d712508e2'
+
+// Weather Endpoint
+export const getWeather = (location) => {
 	return axios({
 		method: 'GET',
-		url: apiUrl + city + '&units=imperial&APPID=' + apiKey,
+		url: weatherApiUrl + location + '&units=imperial&APPID=' + weatherApiKey,
 	})
 }
 
-// Spotify Endpoints
+// News Endpoint
+export const getNews = (location) => {
+	return axios({
+		method: 'GET',
+		url: newsApiUrl + location + newsApiKey,
+	})
+}
+// Spotify Endpoint
 export const getMusicPlayer = () => {
 	return axios({
 		method: 'GET',
